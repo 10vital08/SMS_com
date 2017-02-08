546edcaf3df3ef74e8f6f0b46a99f98e0c3c4f0f
10vital08 <vital.lushnikov@mail.ru>
2017-02-08 14:17:01 +0300

    Осуществлен вывод доступных COM-портов и добавлено событие их обновления.

--- SMS_com/Form1.Designer.cs
+++ SMS_com/Form1.Designer.cs
@@ -40,7 +40,7 @@
             // 
             // maskedTextBox1
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(40, 82);
+            this.maskedTextBox1.Location = new System.Drawing.Point(12, 71);
             this.maskedTextBox1.Mask = "(999) 000-0000";
             this.maskedTextBox1.Name = "maskedTextBox1";
             this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
@@ -49,7 +49,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(40, 13);
+            this.label1.Location = new System.Drawing.Point(12, 8);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 13);
             this.label1.TabIndex = 1;
@@ -57,34 +57,35 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(40, 134);
+            this.textBox1.Location = new System.Drawing.Point(12, 123);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(235, 75);
+            this.textBox1.Size = new System.Drawing.Size(296, 98);
             this.textBox1.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(167, 27);
+            this.button1.Location = new System.Drawing.Point(139, 22);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.Size = new System.Drawing.Size(169, 23);
             this.button1.TabIndex = 3;
-            this.button1.Text = "Обновить список";
+            this.button1.Text = "Обновить список COM-портов";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(40, 215);
+            this.button2.Location = new System.Drawing.Point(12, 227);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(140, 23);
             this.button2.TabIndex = 4;
-            this.button2.Text = "Отправить";
+            this.button2.Text = "Отправить сообщение";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(40, 66);
+            this.label2.Location = new System.Drawing.Point(12, 55);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(101, 13);
             this.label2.TabIndex = 5;
@@ -93,7 +94,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(40, 29);
+            this.comboBox1.Location = new System.Drawing.Point(12, 24);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 6;
@@ -101,7 +102,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(40, 115);
+            this.label3.Location = new System.Drawing.Point(12, 104);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(140, 13);
             this.label3.TabIndex = 7;
@@ -111,7 +112,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(419, 294);
+            this.ClientSize = new System.Drawing.Size(396, 263);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
@@ -122,6 +123,7 @@
             this.Controls.Add(this.maskedTextBox1);
             this.Name = "Form1";
             this.Text = "SMS";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- SMS_com/Form1.cs
+++ SMS_com/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace SMS_com
 {
@@ -15,6 +16,21 @@ namespace SMS_com
         public Form1()
         {
             InitializeComponent();
+            //SerialPort port = new SerialPort("COM3", 9600);//Порт со скоростью 9600 бит в секунду
+             
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            Form1_Load(sender, e);//вызов функции для обновления списка доступных COM-портов
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] myPort; //массив для COM-портов
+            myPort = SerialPort.GetPortNames();//в массив помещаются доступные порты
+            comboBox1.Items.AddRange(myPort);//теперь этот массив заносится в список(comboBox)
         }
     }
 }
